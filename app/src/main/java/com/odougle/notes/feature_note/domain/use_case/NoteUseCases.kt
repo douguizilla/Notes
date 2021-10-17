c4b77fc131244492fab9510d5d629f9268afe681
Douglas Gomes <douglasgommez@gmail.com>
2021-10-17 15:00:02 -0300

    renamed class

--- app/src/main/java/com/odougle/notes/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/odougle/notes/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,6 @@
 package com.odougle.notes.feature_note.domain.use_case
 
-data class NoteUseCase(
+data class NoteUseCases(
     val getNotesUseCase: GetNotesUseCase,
     val deleteNoteUseCase: DeleteNoteUseCase
 )
